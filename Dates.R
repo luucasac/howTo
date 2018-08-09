a9e580f1f7485d39c41ff0894d10e8917fbcb445
Daniel Murta <danielmurta@Daniels-MacBook-Air.local>
2018-08-09 09:19:20 -0300

    Adding some functions to use with lubridate package

--- Dates.R
+++ Dates.R
@@ -1,13 +1,13 @@
 library(lubridate)
 library(readr)
-
+library(magrittr)
 
 #----------------------------------------------------------------------------------------------------#
 #                                     Dates and times
 #                                 Rbase and lubridate packages
 #----------------------------------------------------------------------------------------------------#
 
-
+### R Base date formats
 
 d1 <- "1995-02-02"
 
@@ -25,3 +25,23 @@ dt <- "1992-07-07 04:20:00"
 a <- as.POSIXlt(dt) # list with datetime format
 
 b <- as.POSIXct(dt) # value with datetime format
+
+### Lubridate to format date and datetime
+
+l1 <- ymd(d1) # formating a string with lubridate functions
+
+str(l1) 
+
+l2 <- parse_date_time(d2, orders = "mdy") # generalized method to format dates and datetimes
+
+dates <- data.frame(year = c(1990, 1992, 1995),
+                   month = c(02, 07, 04),
+                   day = c(02, 07, 21))
+
+formated_dates <- make_date(year = data$year, month = data$month, day = data$day) # formating dates in a dataframe in differents columns
+
+str(formated_dates)
+
+make_date(year = 2018, month = 02, day = 20) # example wihtout existing data
+
+
