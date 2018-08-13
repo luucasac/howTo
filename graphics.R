034089a0bedd797629977c3827dfb03b7350dbbf
Daniel Murta <danielmurta@Daniels-MacBook-Air.local>
2018-08-13 13:49:31 -0300

    Graphic to visualize density from temperatures over a mover over a year

--- graphics.R
+++ graphics.R
@@ -3,25 +3,25 @@ library(ggridges)
 library(lubridate)
 library(stringr)
 library(ggridges)
+library(dplyr)
 
 #----------------------------------------------------------------------------------------------------#
 #                                                Graphics
 #                                 R base, plotly, ggplot2 and leaflet
 #----------------------------------------------------------------------------------------------------#
+set.seed(123)
+d <- data.frame(
+  data =sample(seq(as.Date("2010-01-01"), as.Date("2011-01-01"), by = "day"), 365),
+  valueMAX = sample(15:20, 365, replace = T),
+  valueMIN = sample(21:25, 365, replace = T))
 
-
-
-
-
-
-
-
-
+d <- d %>% 
+  mutate(month = month(data, label = T),
+         yday = yday(data)) %>% 
+  arrange(desc(data))
 
 ## ggplot2
 
 ### creating a graphich with temperatures agrupped by the months over the year (INCOMPLETE)
-data <- rbind(beaver1, beaver2)
-
-ggplot(data, aes(x = temp, y = time, heigth = ..density..)) +
+ggplot(d, aes(x = d$valueMAX, y = d$month, height = ..density..)) +
   geom_density_ridges(stat = "density")
